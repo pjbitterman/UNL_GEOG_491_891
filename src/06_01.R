b6fb5ddc2f50c691f138330321bbda89fa9d059e
Patrick Bitterman <patrick.bitterman@gmail.com>
2021-09-28 22:00:00 -0500

    lab 1 fix, 6_01 slides, new data

---
+++ src/06_01.R
@@ -0,0 +1,17 @@
+# Point pattern
+library(tidyverse)
+library(GISTools)
+library(sf)
+library(tmap)
+library(gstat)
+
+#cumulative rainfall for 2008
+precip <- sf::read_sf("./data/Precip2008Readings.shp")
+neb <- sf::read_sf("./data/Nebraska_State_Boundary.shp")
+
+tmap_mode("plot")
+tm_shape(neb) + tm_polygons() + tm_shape(precip) + tm_dots(col='navyblue')
+
+tmap_mode("view")
+tm_shape(neb) + tm_polygons() + tm_shape(precip) + tm_dots(col='navyblue')
+
