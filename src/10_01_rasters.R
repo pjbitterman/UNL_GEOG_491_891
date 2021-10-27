265118307fa7e5b5dd6f0d67db2a0d52bc776ca8
Patrick Bitterman <patrick.bitterman@gmail.com>
2021-10-27 09:47:23 -0500

    10.02 done

--- src/10_01_rasters.R
+++ src/10_01_rasters.R
@@ -60,9 +60,61 @@ poly1 <- rasterToPolygons(myras, dissolve = T)
 tmap_mode("view")
 tm_shape(poly1) + tm_polygons()
 
+### raster math
+
+myras %>% values() %>% range(na.rm = T)
+
+
+myras * 2 
+(myras * 2) %>% values() %>% range(na.rm = T)
+
+myras - 4
+myras ** 2
+log(myras)
+
+# reclassify
+rcl = matrix(c(0, 1, 0, 2, 249, 1, 250, 256, 0), ncol = 3, byrow = TRUE)
+rcl
+
+validdata = reclassify(myras, rcl = rcl)
+validdata
+plot(validdata)
 
 
 
+# then multiply out
+
+validRaster <- myras * validdata
+plot(validRaster)
+
+
+# NOAA transform for CHAMPLAIN data
+# valid as of 2019-02-01 metadata
+transform_champlain_olci <- function(x){
+  
+  10**(((3.0 / 250.0) * x) - 4.2)
+}
+
+
+myras.ci <- validRaster %>% transform_champlain_olci
+plot(myras.ci)
+
+myras_focal = focal(myras.ci, w = matrix(1, nrow = 3, ncol = 3), fun = max)
+plot(myras_focal)
+
+(myras_focal - r_focal) %>% plot
+
+# good practice
+compareRaster(myras_focal, r_focal)
+
+# Global
+
+raster::maxValue(myras.ci)
+raster::minValue(myras.ci)
+
+myras.ci %>% raster::values() %>% mean(na.rm = T)
+
+  
 # for raster math
 
 # ts_2016.0902_0908.L4.LCHMP3.CIcyano.MAXIMUM_7day.tif
